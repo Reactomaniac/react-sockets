88bf56cdc2024461bec27367e245e57200cb0a7c
Junaid Farooq <junaid@evercam.io>
2016-10-24 23:22:35 +0500

    Added first react render

---
+++ app-client.js
@@ -0,0 +1,6 @@
+var React = require("react");
+var ReactDom = require("react-dom");
+
+var APP = require("./components/APP");
+
+ReactDom.render(<APP />, document.getElementById('react-container'));
