fe81abfb60359e8a004af6b4c6e9a07d459bf14e
Junaid Farooq <junaid@evercam.io>
2016-10-25 06:52:47 +0500

    Initiated Socket and connected to sockets

--- components/APP.js
+++ components/APP.js
@@ -1,6 +1,20 @@
 var React = require("react");
 
+var io = require("socket.io-client");
+
 var APP = React.createClass({
+
+
+	componentWillMount() {
+		this.socket = io("http://localhost:3000");
+		this.socket.on("connect", this.connect)
+	},
+
+
+	connect() {
+		alert("Connected: " + this.socket.id)
+	},
+
 	render() {
 		return (<h1> Hello I am from APPjs file </h1>)
 	}
